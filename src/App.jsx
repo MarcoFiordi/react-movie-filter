3efc9a933c71c9cbbda5476e6337b914ecf9c423
Marco Fiordi <marco.fiordi97@gmail.com>
2026-05-06 16:46:11 +0200

    creato lo stato genre e implementata la lista dei film, con la map

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
-import HelloClasse154 from "./components/HelloClasse154";
+import { useEffect, useState } from "react";
+
+const movies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+];
+
+
 
 function App() {
-  return 
+  const [genre, setGenre] = useState('');
+  return(
+    <>
+    <h1>MOVIES LIST</h1>
+    <ul>
+      {movies.map((movie, index) => {
+        return <li key={index} >{movie.title} {movie.genre}</li>
+      })}
+    </ul>
+    </>
+  )
 }
 export default App;
